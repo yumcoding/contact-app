c6ef234e4de39c92fdfbac0008871e68694bcc3e
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-08-19 15:15:29 +0200

    Create PhoneInfo.js

---
+++ src/components/PhoneInfo.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class PhoneInfo extends Component {
+  render() {
+    const { name, phone } = this.props.info;
+    const style = {
+      border: "1px solid black",
+      padding: "8px",
+      margin: "8px",
+    };
+    return (
+      <div style={style}>
+        <div>{name}</div>
+        <div>{phone}</div>
+      </div>
+    );
+  }
+}
+
+export default PhoneInfo;
