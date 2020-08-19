1eeb048d8c5d75098bf40cd73fbfa39dc703603e
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-08-19 10:56:59 +0200

    add data into an array

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import PhoneForm from "./components/PhoneForm";
 
 class App extends React.Component {
+  state = {
+    information: [],
+  };
+
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({ information: information.concat(data) });
   };
 
   render() {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(this.state.information)}
       </div>
     );
   }
