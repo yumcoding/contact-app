232ac905321940545f794a2be78a54cd97bfcdce
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-08-19 10:39:18 +0200

    add handleCreate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import PhoneForm from "./components/PhoneForm";
 
-function App() {
-  return (
-    <div>
-      <PhoneForm />
-    </div>
-  );
+class App extends React.Component {
+  handleCreate = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+      </div>
+    );
+  }
 }
 
 export default App;
